115cf514af9efc3a16ab4550e6600ff9fb38550b
Aleksandr <dnepr03.0304@gmail.com>
2024-06-27 00:02:54 +0300

    vite-project-smile

---
+++ DZ_25.1_Voting_for_a_smiley/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import  './App.css';
+import  Acounnt  from './components/Acounnt.jsx';
+function App() {
+  return (
+   <>
+   <div className='container'>
+    <h1>голосування за найкращий смайлик</h1>
+    <Acounnt/>
+   </div>
+   </> 
+  )
+}
+
+export default App
